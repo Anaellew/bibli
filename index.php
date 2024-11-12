38e9a2edf81a82f307eb6d3b039170e9acb50ec1
Anaëlle <anaellewiersch@gmail.com>
2024-11-12 21:13:56 +0100

    jj

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ma Bibliothèque</title>
+    <title>La Bibliothèque</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
