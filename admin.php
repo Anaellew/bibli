2efc578e68ea07780eaf05569774ed4d5372be81
Anaëlle <anaellewiersch@gmail.com>
2024-11-17 22:29:07 +0100

    yo

---
+++ admin.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>La Bibliothèque</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <header> <h1>La Bibliothèque de Centrale Lille</h1> </header>
+
+    <main>
+
+    <?php
+    ?>
+
+    </main>
+
+    <footer> <p>&copy; 2024 La Bibliothèque</p> </footer>
+
+</body>
+</html>
